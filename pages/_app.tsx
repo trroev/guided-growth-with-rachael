55f77fd1397febe876adf6d216702521c265d50c
Trevor Mathiak <tmathiak@gmail.com>
2023-04-06 23:30:22 -0400

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Work_Sans } from "next/font/google";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 const workSans = Work_Sans({
   subsets: ["latin"],
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={workSans.className}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
