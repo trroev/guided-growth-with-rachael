a88f2e874ab73f2b3a2bac350396ba3c770e84d5
Trevor Mathiak <tmathiak@gmail.com>
2023-08-22 14:08:07 -0400

    Move image above contact form
    Also removed phone number and address

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -9,13 +9,53 @@ const Contact = () => {
   return (
     <Layout title="CONTACT">
       <div className="w-full max-w-[1240px] mx-auto py-12">
-        <div className="flex flex-col px-4">
+        <div className="flex flex-col px-4 mb-3">
           <p className="uppercase text-2xl tracking-widest border-b border-green">
             Contact
           </p>
-          <h2 className="py-4 text-center">Get In Touch</h2>
-          <div className="flex flex-col py-4">
-            <div className="mx-auto px-12 py-3 mb-6 border rounded-md border-green sm:mx-0 sm:px-0 sm:flex sm:justify-around lg:mx-20">
+        </div>
+        <div className="flex flex-col justify-center items-center h-72 w-full mx-auto relative md:h-80 lg:h-96">
+          <Image
+            alt="Poppies with ocean in background"
+            src={poppies}
+            fill
+            placeholder="blur"
+            sizes="100vw"
+            style={{
+              objectFit: "cover",
+              zIndex: "-1",
+            }}
+          />
+          <div className="text-center bg-beige/60 p-6 w-full">
+            <h2 className="py-4 text-center">Get In Touch</h2>
+          </div>
+        </div>
+        <div className="relative">
+          <p className="text-xs absolute top-0 right-1">
+            Photo by{" "}
+            <a
+              className="hover:underline"
+              href="https://unsplash.com/@timmossholder?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Link to Tim Mossholder's Unsplash profile"
+            >
+              Tim Mossholder
+            </a>{" "}
+            on{" "}
+            <a
+              className="hover:underline"
+              href="https://unsplash.com/photos/aiJ0R0oRtak?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Link to the bannger image on unsplash.com"
+            >
+              Unsplash
+            </a>
+          </p>
+        </div>
+        <div className="flex flex-col py-4">
+          {/* <div className="mx-auto px-12 py-3 mb-6 border rounded-md border-green sm:mx-0 sm:px-0 sm:flex sm:justify-around lg:mx-20">
               <p className="flex items-center gap-3 pb-6 sm:pb-0">
                 <Icons.phone size={16} aria-hidden="true" />
                 <a
@@ -42,56 +82,17 @@ const Contact = () => {
                   Grand Rapids, MI 49525
                 </a>
               </p>
-            </div>
-            <p className="py-4 text-center">
-              Please fill out the form below, and I will get back to
-              you within two business days. I look forward to speaking
-              with you!
-            </p>
-          </div>
+            </div> */}
         </div>
+        <p className="py-4 text-center">
+          Please fill out the form below, and I will get back to you
+          within two business days. I look forward to speaking with
+          you!
+        </p>
         <div className="w-full h-auto lg:p-4">
           <div className="p-4">
             <FormikForm />
           </div>
-        </div>
-      </div>
-      <div className="mb-6">
-        <div className="flex flex-col justify-center items-center h-96 w-full mx-auto relative md:h-[32rem] lg:h-[40rem]">
-          <Image
-            alt="poppies with ocean in background"
-            src={poppies}
-            fill
-            placeholder="blur"
-            sizes="100vw"
-            style={{
-              objectFit: "cover",
-            }}
-          />
-        </div>
-        <div className="relative">
-          <p className="text-xs absolute top-0 right-1">
-            Photo by{" "}
-            <a
-              className="hover:underline"
-              href="https://unsplash.com/@timmossholder?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Link to Tim Mossholder's Unsplash profile"
-            >
-              Tim Mossholder
-            </a>{" "}
-            on{" "}
-            <a
-              className="hover:underline"
-              href="https://unsplash.com/photos/aiJ0R0oRtak?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Link to the bannger image on unsplash.com"
-            >
-              Unsplash
-            </a>
-          </p>
         </div>
       </div>
     </Layout>
