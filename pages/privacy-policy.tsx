8e2311cd990745eb952e8da8a3903ff799ff4593
Trevor Mathiak <tmathiak@gmail.com>
2023-11-01 21:43:45 -0400

    Add privacy policy page

---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,171 @@
+import Layout from "@/components/Layout";
+
+const PrivacyPolicy = () => {
+  return (
+    <Layout title="Privacy Policy">
+      <div className="w-full max-w-[1240px] mx-auto py-12">
+        <div className="flex flex-col px-4 mb-3">
+          <p className="uppercase text-2xl tracking-widest border-b border-green">
+            Privacy Policy
+          </p>
+          <div className="flex flex-col py-4">
+            <p className="text-xs pb-4">
+              Last updated: Nov 1, 2023 9:12 PM
+            </p>
+            <div className="pb-6">
+              <p className="pb-4">
+                We value your privacy very highly. Please read this
+                Privacy Policy carefully before using the
+                www.rachaelmathiak.com Website (the
+                &quot;Website&quot;) operated by Rachael Mathiak
+                Therapy LLC, a(n) Sole Proprietorship formed in
+                Michigan, United States (&quot;us,&quot;
+                &quot;we&quot;, &quot;our&quot;) as this Privacy
+                Policy contains important information regarding your
+                privacy and how we may use the information we collect
+                about you.
+              </p>
+              <p className="pb-4">
+                Your access to and use of the Website is conditional
+                upon your acceptance of and compliance with this
+                Privacy Policy. This Privacy Policy applies to
+                everyone, including, but not limited to: visitors,
+                users, and others, who wish to access or use the
+                Website.
+              </p>
+              <p className="pb-4">
+                By accessing or using the Website, you agree to be
+                bound by this Privacy Policy. If you disagree with any
+                part of the Privacy Policy, then you do not have our
+                permission to access or use the Website.
+              </p>
+              <p className="uppercase text-xl tracking-widest border-b border-green mb-4">
+                What Information We Collect
+              </p>
+              <p className="pb-4">
+                We collect any and all information that you enter on
+                this Website. We collect the following information
+                about you:
+              </p>
+              <div className="flex pb-4 gap-6">
+                <p className="font-semibold">
+                  Identifying Information:
+                </p>
+                <p>Name, Phone Number, Email Address</p>
+              </div>
+              <div className="flex pb-4 gap-6">
+                <p className="font-semibold">
+                  Characteristics of protected classifications:
+                </p>
+                <p>Age, Mental Disabilities</p>
+              </div>
+              <div className="flex pb-4 gap-6">
+                <p className="font-semibold">
+                  Internet or other electronic activity:
+                </p>
+                <p>
+                  Information regarding your interaction with our
+                  website
+                </p>
+              </div>
+              <p className="uppercase text-xl tracking-widest border-b border-green mb-4">
+                How we may use your information
+              </p>
+              <p className="pb-4">
+                We may use the information that you provide us for the
+                following:
+              </p>
+              <ul className="px-12 pb-4">
+                <li className="list-disc">Performing services</li>
+                <li className="list-disc">
+                  Providing customer service
+                </li>
+                <li className="list-disc">
+                  Verifying customer information
+                </li>
+                <li className="list-disc">Analytics</li>
+                <li className="list-disc">
+                  Marketing and advertising
+                </li>
+              </ul>
+              <p className="uppercase text-xl tracking-widest border-b border-green mb-4">
+                With whom we share your personal information
+              </p>
+              <p className="pb-4">
+                We do not share your personal information with any
+                third parties.
+              </p>
+              <p className="uppercase text-xl tracking-widest border-b border-green mb-4">
+                Cookies
+              </p>
+              <p className="pb-4">
+                A cookie is a small piece of data sent from a website
+                and stored on your device by your browser. Cookies are
+                generally used to allow websites to respond to you as
+                an individual, to remember your preferences, to test
+                content, and to analyze your interactions with
+                Websites or with advertisements. This Website does not
+                collect cookies.
+              </p>
+              <p className="uppercase text-xl tracking-widest border-b border-green mb-4">
+                Children&quot;s Privacy
+              </p>
+              <p className="pb-4">
+                This Website is intended for use by a general audience
+                and does not offer services to children. Should a
+                child whom we know to be under 18 send personal
+                information to us, we will use that information only
+                to respond to that child to inform him or her that
+                they cannot use this Website.
+              </p>
+              <p className="uppercase text-xl tracking-widest border-b border-green mb-4">
+                Third-Party Websites
+              </p>
+              <p className="pb-4">
+                This Website may contain hyperlinks to websites
+                operated by parties other than us. We provide such
+                hyperlinks for your reference only. We do not control
+                such websites and are not responsible for their
+                contents or the privacy or other practices of such
+                websites. It is up to you to read and fully understand
+                their Privacy Policies. Our inclusion of hyperlinks to
+                such websites does not imply any endorsement of the
+                material on such websites or any association with
+                their operators.
+              </p>
+              <p className="uppercase text-xl tracking-widest border-b border-green mb-4">
+                Do Not Track
+              </p>
+              <p className="pb-4">
+                Do Not Track is a preference you can set on your
+                browser to inform websites that you do not want to be
+                tracked. We do not support Do Not Track
+                (&quot;DNT&quot;). You can either enable or disable Do
+                Not Track by visiting the Preferences or Settings page
+                of your browser.
+              </p>
+              <p className="uppercase text-xl tracking-widest border-b border-green mb-4">
+                Changes to Privacy Policy
+              </p>
+              <p className="pb-4">
+                We reserve the right to amend this Privacy Policy at
+                any time. We will notify you of any changes to this
+                Privacy Policy by posting the updated Privacy Policy
+                to this website or application.
+              </p>
+              <p className="uppercase text-xl tracking-widest border-b border-green mb-4">
+                Questions
+              </p>
+              <p className="pb-4">
+                If you have any questions about this Privacy Policy,
+                please contact us at therapy@rachaelmathiak.com.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default PrivacyPolicy;
